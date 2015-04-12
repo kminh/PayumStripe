fc8233416793575be7b119ee21c3ac3fd9a67082
Khang Minh <mr.minhkhang@gmail.com>
2015-04-13 00:44:51 +0700

    better handling of existing customer

--- Action/Api/CreateCustomerAction.php
+++ Action/Api/CreateCustomerAction.php
@@ -56,25 +56,50 @@ class CreateCustomerAction extends GatewayAwareAction implements ApiAwareInterfa
 
         // if an id is set, we try to retrieve customer first
         if (!empty($model['id'])) {
-            try {
-                $customer = \Stripe_Customer::retrieve($model['id']);
+            $id = $model['id'];
+            unset($model['id']);
 
-                unset($model['id']);
+            try {
+                $customer = \Stripe_Customer::retrieve(array('id' => $id, 'expand' => array('default_source')));
 
                 // create new subscription for this customer
                 try {
                     // in case this customer exists but has no subscription we
                     // can't create new one, must create new customer
                     if (!is_null($customer->subscriptions)) {
+                        // use existing card if it's the same one being submitted
+                        // otherwise Stripe will remove default card and
+                        // replace with this one
+                        if (isset($model['card']) && isset($customer->default_source)) {
+                            // retrieve currently submitted card from token to compare
+                            try {
+                                $submittedCard = \Stripe_Token::retrieve($model['card']);
+
+                                // use currently registered card instead
+                                if ($submittedCard->card->fingerprint == $customer->default_source->fingerprint) {
+                                    unset($model['card']);
+                                }
+                            } catch (\Stripe_Error $e) {
+                                $model->replace($e->getJsonBody());
+                                return;
+                            }
+                        }
+
                         $subscription = $customer->subscriptions->create($model->toUnsafeArray());
                         $model->replace($subscription->__toArray(true));
+
                         return;
                     }
                 } catch (\Stripe_Error $e) {
                     $model->replace($e->getJsonBody());
+                    return;
                 }
-            } catch (\Stripe_Error $e) {
+            } catch (\Stripe_InvalidRequestError $e) {
                 // probably customer does not exist yet
+            } catch (\Stripe_Error $e) {
+                // an unrecoverable error
+                $model->replace($e->getJsonBody());
+                return;
             }
         }
 
